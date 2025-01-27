ae0609d95f5539603217f1b9582bcd78a7b96ef3
stephen249 <stephenmasih39@gmail.com>
2025-01-27 17:53:53 -0500

    Build Folder for deploying

--- random-quote/src/App.jsx
+++ random-quote/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Button from "./components/Button";
 import Card from "./components/Card";
 import CardContent from "./components/CardContent";
